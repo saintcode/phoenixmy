2e157fe299268086acbe86c85c8ba15f4d049ab9
Vadim Evseev <v.evseev@smcs.com.ua>
2016-06-09 18:36:37 +0300

    fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :phoenixmy, Phoenixmy.Endpoint,
   secret_key_base: 'asgiklhjklFGFGFYsdfasdfasdfasdfasdfasKHKUVUGIUFDF'
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :phoenixmy, HelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
