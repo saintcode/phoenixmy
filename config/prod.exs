dabac5059051f0ff33460e6273ea8895f2383ee6
Vadim Evseev <v.evseev@smcs.com.ua>
2016-06-09 18:32:38 +0300

    fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -23,9 +23,7 @@ config :hello_phoenix, HelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true,
-  transport :websocket, Phoenix.Transports.WebSocket,
-  timeout: 45_000
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
